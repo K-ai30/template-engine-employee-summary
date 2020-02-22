106bb63523f8bfba15c41d64d0e460ed2e616e9f
K-ai30 <artpirate11@aim.com>
2020-02-22 16:03:45 -0500

    updated html and javascript

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const Intern = require('./lib/intern');
 
 const team = [];
 
-// Ask all employees these main questions before asking for specific role questions
-function askMainQuestions() {
+// Ask which type of employee question
+function askMainQuestion() {
     const mainQuestions = [
         {
             type: "list",
@@ -15,6 +15,18 @@ function askMainQuestions() {
             name: "roleChoice",
             choices: ["Manager", "Engineer", "Intern", "Done"]
         },
+    ];
+        inquirer
+        .prompt(mainQuestions)
+        .then(answers => {
+            return answers.roleChoice;
+            console.log(answers);
+        })
+}
+
+// Ask all employees these main questions before asking for specific role questions
+function askSecondaryQuestions() {
+    const mainQuestions = [
         {
             type: "input",
             message: "Enter team member's name:",
@@ -31,61 +43,77 @@ function askMainQuestions() {
             name: "employeeEmail",
         }
       ];
+      for(let i = 0; i < mainQuestions.length; i++) {
+        inquirer
+        .prompt(mainQuestions[i])
+        .then(answers => {
+
+        })
+      }
 }
 
 // Ask manager specific questions for role details
 function ManagerRole() {
-    askMainQuestions();
-
-    const managerQuestions = [
-      {
-          type: "input",
-          message: "Enter your office number:",
-          name: "officeNumber"
-      }
-    ];
-
-    inquirer
-    .prompt(managerQuestions)
-    .then(answers => {
-        console.log("Answers: ", answers);
-    })
+    // askMainQuestions().then(function() {
+        const managerQuestions = [
+          {
+              type: "input",
+              message: "Enter your office number:",
+              name: "officeNumber"
+          }
+        ];
+    
+        inquirer
+        .prompt(managerQuestions)
+        .then(answers => {
+            console.log("Answers: ", answers);
+        })
+    // })
 }
 
 // Ask engineer specific questions for role details
 function EngineerRole() {
-    askMainQuestions();
-
-    const engineerQuestions = [
-      {
-          type: "input",
-          message: "Enter engineer's GitHub username:",
-          name: "GHUsername"
-      }
-    ];
-
-    inquirer
-    .prompt(engineerQuestions)
-    .then(answers => {
-        console.log("Answers: ", answers);
-    })
+    // askMainQuestions().then(function() {
+        const engineerQuestions = [
+          {
+              type: "input",
+              message: "Enter engineer's GitHub username:",
+              name: "GHUsername"
+          }
+        ];
+    
+        inquirer
+        .prompt(engineerQuestions)
+        .then(answers => {
+            console.log("Answers: ", answers);
+        })
+    // })
 }
 
 // Ask intern specific questions for role details
 function InternRole() {
-    askMainQuestions();
+    // askMainQuestions().then(function() {
+        const internQuestions = [
+          {
+              type: "input",
+              message: "Enter intern's school:",
+              name: "schoolName"
+          }
+        ];
 
-    const internQuestions = [
-      {
-          type: "input",
-          message: "Enter intern's school:",
-          name: "schoolName"
-      }
-    ];
-
-    inquirer
-    .prompt(internQuestions)
-    .then(answers => {
-        console.log("Answers: ", answers);
-    })
+        inquirer
+        .prompt(internQuestions)
+        .then(answers => {
+            console.log("Answers: ", answers);
+        })
+    // })
 }
+
+// Do one for each of the job roles so I can call this later
+function main() {
+    let role = askMainQuestion();
+    // ManagerRole();
+    // EngineerRole();
+    // Intern();
+}
+main();
