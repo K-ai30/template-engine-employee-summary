777767678e47f2ba9b10f47edc6fae8f1d63025e
K-ai30 <artpirate11@aim.com>
2020-03-25 22:20:56 -0400

    updated functionality

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const Intern = require('./lib/intern');
 
 const team = [];
 
-// Ask which type of employee question
-function askMainQuestion() {
+// Ask all employees these main questions before asking for specific role questions
+function askMainQuestions() {
     const mainQuestions = [
         {
             type: "list",
@@ -15,18 +15,7 @@ function askMainQuestion() {
             name: "roleChoice",
             choices: ["Manager", "Engineer", "Intern", "Done"]
         },
-    ];
-        inquirer
-        .prompt(mainQuestions)
-        .then(answers => {
-            return answers.roleChoice;
-            console.log(answers);
-        })
-}
-
-// Ask all employees these main questions before asking for specific role questions
-function askSecondaryQuestions() {
-    const mainQuestions = [
+        // Ask all employees these main questions before asking for specific role questions
         {
             type: "input",
             message: "Enter team member's name:",
@@ -42,14 +31,25 @@ function askSecondaryQuestions() {
             message: "Enter team member's email:",
             name: "employeeEmail",
         }
-      ];
-      for(let i = 0; i < mainQuestions.length; i++) {
+    ];
         inquirer
-        .prompt(mainQuestions[i])
+        .prompt(mainQuestions)
         .then(answers => {
-
+            console.log('Answers: ', answers);
+            if (answers.roleChoice === 'Manager') {
+                ManagerRole(answers);
+                console.log('Manager title');
+            } else if (answers.roleChoice === 'Engineer') {
+                EngineerRole(answers);
+                console.log('Engineer title');
+            } else if (answers.roleChoice === 'Intern') {
+                InternRole(answers);
+                console.log('Intern title');
+            } else {
+                console.log('I am done!');
+            }
+            return answers.roleChoice;  //end of the function
         })
-      }
 }
 
 // Ask manager specific questions for role details
@@ -72,7 +72,7 @@ function ManagerRole() {
 }
 
 // Ask engineer specific questions for role details
-function EngineerRole() {
+function EngineerRole(mainAnswers) {
     // askMainQuestions().then(function() {
         const engineerQuestions = [
           {
@@ -85,13 +85,50 @@ function EngineerRole() {
         inquirer
         .prompt(engineerQuestions)
         .then(answers => {
-            console.log("Answers: ", answers);
+            team.push({
+                title: mainAnswers.roleChoice,
+                name: mainAnswers.employeeName,
+                id: mainAnswers.employeeID,
+                email: mainAnswers.employeeEmail
+            })
         })
     // })
 }
 
+// If the user is finished, then stop the program, or ask the main questions again if they are not.
+const finishQuestions = [
+    {
+        type: "checkbox",
+        message: "Are you finished?",
+        name: "isComplete",
+        choices: ['Yes', 'No']
+    }
+];
+inquirer
+.prompt(finishQuestions)
+.then(function(data) {
+    console.log('Data: ', data);
+    if (data.isComplete[0] === 'Yes') {
+        const engineerData = generateTemplate(team);  // generate template
+        createRoleFile(engineerData);
+        console.log("Answers: ", answers);
+    } else {
+        askMainQuestion();
+    }
+})
+
+function createRoleFile(data) {
+    fs.writeFile('./output/team.html', data, 'UTF-8', function(error, data) {
+        if (error) {
+            console.log('File cannot be written!');
+        } else {
+            console.log('File written!');
+        }
+    })
+}
+
 // Ask intern specific questions for role details
-function InternRole() {
+function InternRole(mainAnswers) {
     // askMainQuestions().then(function() {
         const internQuestions = [
           {
@@ -109,11 +146,27 @@ function InternRole() {
     // })
 }
 
-// Do one for each of the job roles so I can call this later
 function main() {
-    let role = askMainQuestion();
+    let team = [] // array of member attributes,
+    while (input != 'done') {
+        let role = askMainQuestions()
+    }
+    members = [];
+    for (member in team){
+        members.push(makeMember(member));
+    }
+    memberTemplates = [];
+    for (member in members){
+        memberTemplate.push(makeMemberTemplate(member));
+    }
+    for(template in memberTemplates){
+        // add them to the layout
+    }
+    //fs.write(layout) to the File.
+    // let role = askMainQuestion();
     // ManagerRole();
     // EngineerRole();
     // Intern();
 }
+
 main();
